6e6870ded3555e817c4e21ceca3ad2fe08945b23
Rahul Rimal <genuinetechnology9@gmail.com>
2022-07-31 08:36:29 +0545

    Working on models

--- v1/models.py
+++ v1/models.py
@@ -1,3 +1,16 @@
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+
+
+
+class Book(models.Model):
+    book_name = models.CharField(max_length=256, null=False)
+    author = models.CharField(max_length=256, null=True, blank= True)
+    description = models.CharField(max_length=256, null=True, blank= True)
+    bought_date = models.DateField(auto_now_add=True)
+    price = models.DecimalField(decimal_places=2, max_digits=8)
+    # cover_picture = 
+    # pictures = 
+    created_on = models.DateTimeField(auto_now_add=True)
